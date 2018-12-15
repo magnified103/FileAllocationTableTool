7e1e36d03238697f669dfacab8f921ee9e29c5e6
dung11112003 <dung11112003@gmail.com>
2018-12-15 17:54:57 +0700

    beta

--- FileAllocationTableTool/Layout/RootDirectoryRegion.cs
+++ FileAllocationTableTool/Layout/RootDirectoryRegion.cs
@@ -1,10 +1,12 @@
-﻿namespace FileAllocationTableTool.Layout
+﻿using System.Collections.Generic;
+
+namespace FileAllocationTableTool.Layout
 {
     /*
         Root Directory region
     */
     class RootDirectoryRegion
     {
-
+        List<DirectoryEntry> DirectoryEntries = new List<DirectoryEntry>();
     }
 }
