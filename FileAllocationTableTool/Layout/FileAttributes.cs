5332fb1ac31d5f39fc259f65e5ccfe79a0917877
dung11112003 <dung11112003@gmail.com>
2018-12-16 20:48:07 +0700

    Fixes

---
+++ FileAllocationTableTool/Layout/FileAttributes.cs
@@ -0,0 +1,40 @@
+﻿using FileAllocationTableTool.Tools;
+
+namespace FileAllocationTableTool.Layout
+{
+    class FileAttributes
+    {
+        bool ReadOnly;
+        bool Hidden;
+        bool System;
+        bool VolumeLabel;
+        bool Subdirectory;
+        bool Archive;
+        bool Device;
+        bool Reserved;
+
+        public FileAttributes()
+        {
+            ReadOnly = new bool();
+            Hidden = new bool();
+            System = new bool();
+            VolumeLabel = new bool();
+            Subdirectory = new bool();
+            Archive = new bool();
+            Device = new bool();
+            Reserved = new bool();
+        }
+
+        public FileAttributes(byte In)
+        {
+            ReadOnly = Converter.ReadBitAtPosition(In, 1);
+            Hidden = Converter.ReadBitAtPosition(In, 2);
+            System = Converter.ReadBitAtPosition(In, 3);
+            VolumeLabel = Converter.ReadBitAtPosition(In, 4);
+            Subdirectory = Converter.ReadBitAtPosition(In, 5);
+            Archive = Converter.ReadBitAtPosition(In, 6);
+            Device = Converter.ReadBitAtPosition(In, 7);
+            Reserved = Converter.ReadBitAtPosition(In, 8);
+        }
+    }
+}
